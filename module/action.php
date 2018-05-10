3f897f831d31b0de844ff116a181c802172c556a
SuperKongBrian <superkongbrian@gmail.com>
2018-05-10 14:37:36 +1200

    [modify] onGoing chatBot possible now

--- module/action.php
+++ module/action.php
@@ -1,24 +1,41 @@
 <textarea readonly>
-ChatBot Intel: Hello. How can I help you?
 <?php
-	if ($_SERVER['REQUEST_METHOD'] === 'GET' && htmlspecialchars($_GET['messanger']) != null) {
-		echo 'You: '. htmlspecialchars($_GET['messanger']);
-		echo '&#13;&#10;ChatBot Intel: '. 'Thank you for your cooperation. cookie.';
-	}
-?>
-</textarea>
-<?php
-if ($_SERVER['REQUEST_METHOD'] === 'GET' && htmlspecialchars($_GET['messanger']) != null) {
-	echo "<form action=\"chatform.php\" method=\"get\">";
-	echo "<input class=\"input\" name=\"messanger\" type=\"text\" readonly>";
-	echo "<input class=\"input-button\" value=\"Submit\" type=\"submit\" disabled>";
+
+$temp = null;
+
+if(!isset($_COOKIE["chatBotIntel"])) {
+	$temp = 'ChatBot Intel: Hello. How can I help you?';
+	setcookie("chatBotIntel", $temp);
+	echo $temp;
 }
 else
 {
-	echo "<form action=\"chatform.php\" method=\"get\">";;
+	echo $_COOKIE["chatBotIntel"];
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'GET' && htmlspecialchars($_GET['messanger']) != null) {
+	$tempPrint1 = "&#13;&#10;You: " . htmlspecialchars($_GET['messanger']);
+	$tempPrint2 = "&#13;&#10;ChatBot Intel: ". "Thank you for your cooperation. cookie.";
+
+	echo $tempPrint1;
+	echo $tempPrint2;
+
+	if($temp == null)
+	{
+		$temp = $_COOKIE["chatBotIntel"];
+		setcookie("chatBotIntel", $temp . $tempPrint1 . $tempPrint2);
+	}
+	else
+	{
+		setcookie("chatBotIntel", $temp . $tempPrint1 . $tempPrint2);
+	}
+}
+?>
+</textarea>
+<?php
+	echo "<form action=\"chatform.php\" method=\"get\">";
 	echo "<input class=\"input\" name=\"messanger\" type=\"text\">";
 	echo "<input class=\"input-button\" value=\"Submit\" type=\"submit\">";
-}
 ?>
 
 </form>
